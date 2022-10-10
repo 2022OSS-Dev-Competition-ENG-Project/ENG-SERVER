0d9ca7e5bff0dad84471ec882399fc90d7fb7fdc
jeong <ukidd12@naver.com>
2022-10-10 19:24:38 +0900

    [Test] Facility

--- facility-service/src/main/java/com/example/facilityservice/constant/QrConstant.java
+++ facility-service/src/main/java/com/example/facilityservice/constant/QrConstant.java
@@ -4,10 +4,10 @@ public class QrConstant {
 
     /* QR 코드 생성 셋팅 */
     public static final String QR_CODE_GENERATE_URL = "$id/$name/$address";
-    public static final String QR_CODE_SAVE_PATH = "/home/eng2/ENG-STORE/image/QR/";
-    public static final String QR_CODE_SAVE_PATH_DB = "http://jlchj.iptime.org:1001/api/facility/content/image/view/home&icslab&ENG-store&image&QR&";
-//    public static final String QR_CODE_SAVE_PATH = "/Users/jeonghunlee/qr";
-//    public static final String QR_CODE_SAVE_PATH_DB = "http://203.250.32.29:2200/api/facility/content/image/view/Users&jeonghunlee&qr&";
+//    public static final String QR_CODE_SAVE_PATH = "/home/eng2/ENG-STORE/image/QR/";
+//    public static final String QR_CODE_SAVE_PATH_DB = "http://jlchj.iptime.org:1001/api/facility/content/image/view/home&icslab&ENG-store&image&QR&";
+    public static final String QR_CODE_SAVE_PATH = "/Users/jeonghunlee/image/qr";
+    public static final String QR_CODE_SAVE_PATH_DB = "http://203.250.32.29:2200/api/facility/content/image/view/Users&jeonghunlee&qr&";
 
 
     public static final int QR_CODE_WIDTH = 200;

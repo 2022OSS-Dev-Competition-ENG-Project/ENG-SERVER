6c81d5edd520de68ba3ecf24e1276008c7ff36ea
jeong <ukidd12@naver.com>
2022-10-13 20:52:04 +0900

    [fix] api gateway

--- facility-service/src/main/java/com/example/facilityservice/constant/FacilityReportConstant.java
+++ facility-service/src/main/java/com/example/facilityservice/constant/FacilityReportConstant.java
@@ -2,8 +2,8 @@ package com.example.facilityservice.constant;
 
 public class FacilityReportConstant {
 
-        public static final String REPORT_IMAGES_SAVE_PATH = "/Users/jeonghunlee/image/content/";
-//    public static final String REPORT_IMAGES_SAVE_PATH = "/home/icslab/ENG-store/image/report/";
+        public static final String REPORT_IMAGES_SAVE_PATH = "/Users/jeonghunlee/image/content/report";
+//    public static final String REPORT_IMAGES_SAVE_PATH = "/home/eng2/ENG-STORAGE/images/report/";
         public static final String REPORT_IMAGES_SAVE_DB_PATH = "http://localhost:2200/api/facility/content/image/view/Users&jeonghunlee&image&content&";
 //    public static final String REPORT_IMAGES_SAVE_DB_PATH = "http://203.250.32.29:2200/api/facility/content/image/view/home&icslab&ENG-store&image&report&";
 

721a4a44c6a15b03d3d154d0205582e16f5a088e
jeong <ukidd12@naver.com>
2022-10-13 20:32:20 +0900

    [fix] cors

--- apigateway-service/src/main/java/com/example/apigatewayservice/ApigatewayServiceApplication.java
+++ apigateway-service/src/main/java/com/example/apigatewayservice/ApigatewayServiceApplication.java
@@ -1,13 +1,36 @@
 package com.example.apigatewayservice;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
 @SpringBootApplication
 public class ApigatewayServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ApigatewayServiceApplication.class, args);
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.addAllowedMethod("*");
+        corsConfiguration.setAllowCredentials(true);
+        source.registerCorsConfiguration("/**", corsConfiguration);
+        return source;
+    }
+
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+        return new CorsWebFilter(corsConfigurationSource());
     }
 
 }

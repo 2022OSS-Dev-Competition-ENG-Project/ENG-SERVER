ae177e6f6c7dfd573682ea7c4956092e63f6a71e
jeong <ukidd12@naver.com>
2022-10-14 04:36:13 +0900

    [fix] manager-cors fail..

--- manager-service/src/main/java/com/example/managerservice/controller/ManagerController.java
+++ manager-service/src/main/java/com/example/managerservice/controller/ManagerController.java
@@ -15,7 +15,6 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("")
-@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class ManagerController {
 
     @Autowired

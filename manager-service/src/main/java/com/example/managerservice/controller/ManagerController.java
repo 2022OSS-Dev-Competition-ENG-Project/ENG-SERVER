2cff6b9b754acaaa9e7c3a5fade5232ffd980735
jeong <ukidd12@naver.com>
2022-10-13 19:50:34 +0900

    [fix] cors

--- manager-service/src/main/java/com/example/managerservice/controller/ManagerController.java
+++ manager-service/src/main/java/com/example/managerservice/controller/ManagerController.java
@@ -7,16 +7,15 @@ import com.example.managerservice.vo.RequestChangePassword;
 import com.example.managerservice.vo.RequestFindManagerId;
 import com.example.managerservice.vo.RequestFindManagerPassword;
 import com.example.managerservice.vo.RequestManagerLogin;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
-@Slf4j
+
 @RestController
 @RequestMapping("")
-@ControllerAdvice
+@CrossOrigin("*")
 public class ManagerController {
 
     @Autowired

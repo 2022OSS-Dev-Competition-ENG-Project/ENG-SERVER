67fb84bb604bd698e028fedcdb458c14017b7700
jeong <ukidd12@naver.com>
2022-10-14 15:44:55 +0900

    [fix] report-savLocation

--- facility-service/src/main/java/com/example/facilityservice/constant/FacilityReportConstant.java
+++ facility-service/src/main/java/com/example/facilityservice/constant/FacilityReportConstant.java
@@ -3,7 +3,7 @@ package com.example.facilityservice.constant;
 public class FacilityReportConstant {
 
     /* SERVER */
-        public static final String REPORT_IMAGES_SAVE_PATH = "home/eng2/ENG-STORAGE/images/report/";
+        public static final String REPORT_IMAGES_SAVE_PATH = "/home/eng2/ENG-STORAGE/images/report/";
         public static final String REPORT_IMAGES_SAVE_DB_PATH = "http://jlchj.iptime.org:8000/facility-service/image/view/home&eng2&ENG-STORAGE&images&report&";
 
     /* Local */

9a8a5564b71e528926e69fe97927d64ee7bceb08
jeong <ukidd12@naver.com>
2022-10-16 19:31:17 +0900

    [fix] facility-service

--- facility-service/src/main/java/com/example/facilityservice/FacilityServiceApplication.java
+++ facility-service/src/main/java/com/example/facilityservice/FacilityServiceApplication.java
@@ -5,10 +5,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @EnableDiscoveryClient
 @SpringBootApplication
 @EnableFeignClients
 public class FacilityServiceApplication {
+
+    @PostConstruct
+    void started(){
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/seoul"));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(FacilityServiceApplication.class, args);
--- manager-service/src/main/java/com/example/managerservice/ManagerServiceApplication.java
+++ manager-service/src/main/java/com/example/managerservice/ManagerServiceApplication.java
@@ -6,13 +6,22 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @EnableEurekaClient
 @SpringBootApplication
 public class ManagerServiceApplication {
 
+    @PostConstruct
+    void started(){
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/seoul"));
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(ManagerServiceApplication.class, args);
     }
+
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder(){
         return new BCryptPasswordEncoder();
